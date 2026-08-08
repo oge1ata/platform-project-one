d1a8d6cb34058bb70674ba58d37aba8117b70e35
Ogechukwu Ata <yoma@Mac-2.powerhub>
2026-08-08 01:00:01 +0100

    First Commit

---
+++ app.py
@@ -0,0 +1,11 @@
+# app.py
+from fastapi import FastAPI
+app = FastAPI()
+
+@app.get("/health")
+def health():
+    return {"status": "ok"}
+
+@app.get("/")
+def root():
+    return {"message": "hello"}
